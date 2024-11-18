0e6888abc6dc43deeab4e7eecb4993ff2a46ee42
im-bhuvi <bhuvanesh.bhuvi1606@gamil.com>
2024-11-18 18:13:18 +0530

    deleted

--- src/org/test/Gitpractice.java
+++ src/org/test/Gitpractice.java
@@ -7,21 +7,7 @@ public class Gitpractice {
 	public static void main(String[] args) {
 		int a=10;
 		
-		for(int i=0;i<a;i++) {
-		System.out.println(i);
-	}
-		if (a==11) {
-			System.out.println("correct");
-		}
-		else {
-			if (a>11) {
-				System.out.println("mmm");
-			}
-			else {
-				System.out.println("not");
-			}
-		}
-		
+	System.out.println(a);
 	}
 
 }
