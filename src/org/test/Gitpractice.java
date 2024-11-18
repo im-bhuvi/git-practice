62dc4d84b92b2c7c58e987e27a11463be7683a38
im-bhuvi <bhuvanesh.bhuvi1606@gamil.com>
2024-11-18 17:51:01 +0530

    practice

---
+++ src/org/test/Gitpractice.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import java.util.Iterator;
+
+public class Gitpractice {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		for(int i=0;i<a;i++) {
+		System.out.println(i);
+	}
+		if (a==11) {
+			System.out.println("correct");
+		}
+		else {
+			if (a>11) {
+				System.out.println("mmm");
+			}
+			else {
+				System.out.println("not");
+			}
+		}
+		
+	}
+
+}
